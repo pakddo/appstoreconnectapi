88e3381330b2c4bf43a4b7505a7e9fe8b1fe4c60
Dooho Park <pakddo@pakddo.net>
2024-08-19 19:05:35 +0900

    Update api.py

--- appstoreconnect/api.py
+++ appstoreconnect/api.py
@@ -79,7 +79,7 @@ class Api:
 		self.token_gen_date = datetime.now()
 		exp = int(time.mktime((self.token_gen_date + timedelta(minutes=20)).timetuple()))
 		return jwt.encode({'iss': self.issuer_id, 'exp': exp, 'aud': 'appstoreconnect-v1'}, key,
-		                   headers={'kid': self.key_id, 'typ': 'JWT'}, algorithm=ALGORITHM).decode('ascii')
+		                   headers={'kid': self.key_id, 'typ': 'JWT'}, algorithm=ALGORITHM)
 
 	def _get_resource(self, Resource, resource_id):
 		url = "%s%s/%s" % (BASE_API, Resource.endpoint, resource_id)
